2b727367fd227249aedd720ae62e56ba20ffb981
Felipe Palazzo <palazzofelipe@gmail.com>
2017-02-12 13:02:28 -0200

    Config firebase

--- src/app/components/auth/auth.module.js
+++ src/app/components/auth/auth.module.js
@@ -2,4 +2,19 @@ angular
   .module('components.auth', [
     'ui.router',
     'firebase'
-  ]);
+  ])
+  .config(function ($firebaseRefProvider) {
+    var config = {
+      apiKey: 'AIzaSyDEvB8bumPgXXqJVb-vG00RiOEqdAehiGw',
+      authDomain: 'contacts-manager-c6719.firebaseapp.com',
+      databaseURL: 'https://contacts-manager-c6719.firebaseio.com',
+      storageBucket: 'contacts-manager-c6719.appspot.com',
+      messagingSenderId: '652446250506'
+    };
+    $firebaseRefProvider
+      .registerUrl({
+        default: config.databaseURL,
+        contacts: config.databaseURL + '/contacts'
+      })
+    firebase.initializeApp(config);
+  });
