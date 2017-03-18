b91f23e2fd4feedc2c9919f24341b6cb38b06423
Felipe Palazzo <palazzofelipe@gmail.com>
2017-03-18 15:41:13 -0300

    Create register component

---
+++ src/app/components/auth/register/register.controller.js
@@ -0,0 +1,17 @@
+function RegisterController() {
+  var ctrl = this;
+  ctrl.$onInit = function () {
+    ctrl.error = null;
+    ctrl.user = {
+      email: '',
+      password: '',
+    };
+  };
+  ctrl.createUser = function (event) {
+
+  };
+}
+
+angular
+  .module('components.auth')
+  .controller('RegisterController', RegisterController);
